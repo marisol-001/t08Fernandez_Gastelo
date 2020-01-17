e69780778718df1a8e5dedd466b9c18348569915
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2020-01-17 15:55:43 -0500

    se ingreso libreria

---
+++ libreria.py
@@ -0,0 +1,124 @@
+#EJERCICIO 1
+def cm(cad):
+    if(cad=="Veronica"):
+        return cad.upper()
+    else:
+        return cad
+#FIN
+
+#EJERCICIO 2
+def producto(pr):
+    if(pr=="arroz"):
+        return pr.title()
+    else:
+        return pr
+#FIN
+
+#EJERCICIO 3
+
+def amor(str):
+    if(str=="Mariela"):
+        return str.lower()
+    else:
+        return str
+
+#FIN
+
+#EJERCICIO 4
+def flores(fl):
+    if(fl=="Tulipan"):
+        return fl.replace()
+    else:
+        return fl
+#FIN
+
+#EJERCICIO 5
+def cris(cr):
+    if(cr=="alegria"):
+        return cr.strip()
+    else:
+        return cr
+#FIN
+
+#ejercicio 6
+def bebe(br):
+    if(br=="rous"):
+        return br.lstrip()
+    else:
+        return br
+#FIN
+
+#ejercicio 7
+def basura(ba):
+    if(ba=="zancudo"):
+        return ba.rstrip()
+    else:
+        return ba
+#FIN
+
+#EJERCICIO 8
+def cool(co):
+    if(co=="Sofia"):
+        return co.split()
+    else:
+        return co
+
+#EJERCICIO 9
+def mario(vc):
+    if(vc=="234"):
+        return vc.isdigit()
+    else:
+        return vc
+#FIN
+
+#EJERCICIO 10
+def riycr(tu):
+    if(tu=="345ABC"):
+        return tu.isalnum()
+    else:
+        return tu
+#FIN
+
+#EJERCICIO 11
+def tro(tr):
+    if(tr=="DRE"):
+        return tr.isalpha()
+    else:
+        return tr
+#FIN
+
+#EJERCICIO 12
+def mar(mr):
+    if(mr=="tazon2"):
+        return mr.isascii()
+    else:
+        return mr
+#FIN
+
+#EJERCICIO 13
+def chiquita(cad):
+    if(cad=="nena"):
+        return cad.upper()
+    else:
+        return cad
+#FIN
+
+#EJERCICIO 14
+def papa(pr):
+    if(pr=="hermano"):
+        return pr.title()
+    else:
+        return pr
+
+#FIN
+
+#EJERCICIO 15
+
+def pequeño(str):
+    if(str=="NIÑO"):
+        return str.lower()
+    else:
+        return str
+
+
+#FIN
