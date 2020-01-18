2e48f1b42f9990415423cce7f0a9136e563d300f
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2020-01-17 20:43:08 -0500

    se ingreso test

---
+++ test.py
@@ -0,0 +1,78 @@
+import libreria
+#ejercicio 1
+assert (libreria.cm("veronica")=="VERONICA")
+assert (libreria.cm("mirella")=="mirella")
+print("cm")
+
+#ejercicio 2
+assert (libreria.producto("arroz")=="arroz")
+assert (libreria.producto("mermelada")=="mermelada")
+print("producto")
+
+#ejercicio 3
+
+assert (libreria.mujer("Mariela")=="mariela")
+assert (libreria.mujer("Sonia")=="sonia")
+print("str")
+
+#ejercicio 4
+
+assert (libreria.flores("tulipan1")=="tulipan1")
+assert (libreria.flores("rosa")=="rosa")
+print("fl")
+
+#ejercicio 5
+assert (libreria.cris("alegria")=="alegria")
+assert (libreria.cris("mesa")=="mesa")
+print("cr")
+
+#ejercicio 6
+assert (libreria.bebe("rous")=="rous")
+assert (libreria.bebe("lila")=="lila")
+print("bebe")
+
+#EJERICIO 7
+assert (libreria.basura("zancudo")=="zancudo")
+assert (libreria.basura("avejas")=="avejas")
+print("basura")
+
+#EJERCICIO 8
+assert (libreria.cool(" sofia ")==" sofia ")
+assert (libreria.cool(" brando ")==" brando ")
+print("cool")
+
+#EJERCICIO 9
+assert (libreria.mario(234)==234)
+assert (libreria.mario("455")=="455")
+print("mario")
+
+#EJERCICIO 10
+assert (libreria.cod("345ABC")=="345ABC")
+assert (libreria.cod("678G")=="678G")
+print("cod")
+
+#EJERCICIO 11
+assert (libreria.trig("DRE")=="dre")
+assert (libreria.trig("hit")=="HIT")
+print("trig")
+
+#EJERCICIO 12
+assert (libreria.mar("arena2")=="arena2")
+assert (libreria.mar("sol")=="sol")
+print("mar")
+
+#ejercicio 13
+assert (libreria.chiquita("nena")=="NENA")
+assert (libreria.chiquita("marita")=="marita")
+print("chiquita")
+
+#ejercicio 14
+assert (libreria.papa("hermano")=="hermano")
+assert (libreria.papa("amador")=="amador")
+print("papa")
+
+#ejercicio 15
+
+assert (libreria.pequeño("niño")=="niño")
+assert (libreria.pequeño("mama")=="mama")
+print("pequeño")
